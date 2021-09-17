78a4679425e9576db090ba0a2e574e3faa83e27b
Карепин Денис <HTMLDEN55@gmail.com>
2021-09-17 23:25:15 +0300

    all tasks done

---
+++ Isu/Objects/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Isu.Objects
+{
+    public class Student
+    {
+        private static int _counter;
+
+        public Student()
+        {
+            Name = string.Empty;
+            Groupname = new Group();
+            Id = 100000;
+        }
+
+        public Student(string name, Group groupname)
+        {
+            int id = ++_counter;
+            Name = name;
+            Groupname = groupname;
+            Id = 100000 + id;
+        }
+
+        public string Name { get; }
+        public Group Groupname { get; set; }
+
+        public int Id { get; }
+
+        public void ChangeGroup(Student student, Group newgroup)
+        {
+            student.Groupname = newgroup;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Группа: {Groupname.Name} ФИ: {Name} ID: {Id}");
+        }
+    }
+}
--- Isu/Program.cs
+++ Isu/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Isu
+﻿using System.Collections.Generic;
+using Isu.Objects;
+using Isu.Services;
+
+namespace Isu
 {
     internal class Program
     {
         private static void Main()
         {
+            var isu = new IsuService();
+            Group group = isu.AddGroup("M3207");
+            Group group2 = isu.AddGroup("M3201");
+            Student first = isu.AddStudent(group, "Карепин Денис");
+            first.GetInfo();
+            isu.ChangeStudentGroup(first, group2);
+            first.GetInfo();
+            group.GetInfo();
+            group2.GetInfo();
         }
     }
 }
